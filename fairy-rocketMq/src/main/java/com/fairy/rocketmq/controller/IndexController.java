d2733d03f56e4930e001ab4e6501399e73a0d6a6
hunaglulu <hll10120212@sina.com>
2022-07-11 22:08:27 +0800

    代码提交-rocketmq 事务消息发送 监听回调检查

--- fairy-rocketMq/src/main/java/com/fairy/rocketmq/controller/IndexController.java
+++ fairy-rocketMq/src/main/java/com/fairy/rocketmq/controller/IndexController.java
@@ -4,23 +4,24 @@ import com.alibaba.fastjson.JSON;
 import com.fairy.common.response.CommonResponse;
 import com.fairy.rocketmq.domain.Order;
 import com.fairy.rocketmq.service.MqSender;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.support.GenericMessage;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author hll
  * @version 1.0
  * @date 2022/7/6 14:30
  */
+@Slf4j
 @RestController
 @RequestMapping("/index")
 public class IndexController {
@@ -40,8 +41,34 @@ public class IndexController {
         order.setOrderAmount(12.0);
         order.setOrderId(12);
         order.setProductNum(12);
-        sender.sendMessage(TOPIC_NAME,JSON.toJSONString(order));
+        sender.sendMessage(TOPIC_NAME, JSON.toJSONString(order));
         return CommonResponse.success();
 
     }
+
+    @GetMapping("/transaction")
+    public CommonResponse sendMessageInTransaction() throws InterruptedException {
+        String[] tags = new String[]{"TagA", "TagB", "TagC", "TagD", "TagE"};
+        for (int i = 0; i < 10; i++) {
+            Order order = new Order();
+            order.setOrderAmount(12.0);
+            order.setOrderId(12+i);
+            order.setProductNum(12+i);
+            //尝试在Header中加入一些自定义的属性。
+            Message<String> message = MessageBuilder.withPayload(JSON.toJSONString(order))
+                    .setHeader(RocketMQHeaders.TRANSACTION_ID, "TransID_" + i)
+                    //发到事务监听器里后，这个自己设定的TAGS属性会丢失。但是上面那个属性不会丢失。
+                    .setHeader(RocketMQHeaders.TAGS, tags[i % tags.length])
+                    //MyProp在事务监听器里也能拿到，为什么就单单这个RocketMQHeaders.TAGS拿不到？这只能去调源码了。
+                    .setHeader("MyProp", "MyProp_" + i)
+                    .build();
+            String destination = TOPIC_NAME + ":" + tags[i % tags.length];
+            //这里发送事务消息时，还是会转换成RocketMQ的Message对象，再调用RocketMQ的API完成事务消息机制。
+            SendResult sendResult = rocketMQTemplate.sendMessageInTransaction(destination, message, destination);
+            log.info("事务消息发送结果:{}", sendResult);
+            Thread.sleep(10);
+        }
+        return CommonResponse.success();
+    }
+
 }
